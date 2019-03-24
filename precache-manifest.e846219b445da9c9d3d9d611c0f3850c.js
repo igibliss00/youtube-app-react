3204563baa57fd9b3cce39575c88b9db0440c3db
J <socept@gmail.com>
2019-03-23 22:28:39 -0400

    Updates

---
+++ precache-manifest.e846219b445da9c9d3d9d611c0f3850c.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "2a4f09f0579a65069b88",
+    "url": "/youtube-app-react/static/js/runtime~main.f375142e.js"
+  },
+  {
+    "revision": "22567b9c7f48da29d5fd",
+    "url": "/youtube-app-react/static/js/main.74a715b2.chunk.js"
+  },
+  {
+    "revision": "e053a1da17cbfeebd8e3",
+    "url": "/youtube-app-react/static/js/2.9176d4f9.chunk.js"
+  },
+  {
+    "revision": "22567b9c7f48da29d5fd",
+    "url": "/youtube-app-react/static/css/main.fa007895.chunk.css"
+  },
+  {
+    "revision": "0e1f895dc90eabf54c37ad1109e65e1c",
+    "url": "/youtube-app-react/index.html"
+  }
+];
